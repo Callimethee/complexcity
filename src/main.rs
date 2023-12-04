8915188099e44b9713290b9a5773b97731b29458
Cyprien <cyprien.gille@orange.fr>
2023-12-04 17:02:47 +0100

    Setup trunk

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+// disable console on windows for release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod camera;
 
 use bevy::prelude::*;
 use camera::Camera2dPlugin;
 
-const BG_COLOR: Color = Color::rgb(0.1, 0.1, 0.2);
-
 fn main() {
     App::new()
-        // Built-ins
-        .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(BG_COLOR))
-        // Custom Plugins
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.3)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Complexcity".to_string(),
+                canvas: Some("#bevy".to_owned()),
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(Camera2dPlugin)
         .run();
 }
