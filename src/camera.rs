ee88b5427c552ca2d8a4a7607c64d343834e0026
Cyprien <cyprien.gille@orange.fr>
2023-12-04 22:05:34 +0100

    Add option to zoom

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::states::GameState;
+
 // for z-ordering
 const CAMERA_LEVEL: f32 = 20.0;
 
@@ -7,7 +9,8 @@ pub struct Camera2dPlugin;
 
 impl Plugin for Camera2dPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, zoom_camera.run_if(in_state(GameState::Playing)));
     }
 }
 
@@ -20,4 +23,16 @@ fn spawn_camera(mut commands: Commands) {
         },
         ..default()
     });
+}
+
+fn zoom_camera(mut camera_query: Query<&mut Transform, With<Camera2d>>, keys: Res<Input<KeyCode>>) {
+    let mut camera_transform = camera_query
+        .get_single_mut()
+        .expect("More than one Camera2d!");
+
+    if keys.just_pressed(KeyCode::PageUp) {
+        camera_transform.scale += Vec3::new(0.2, 0.2, 0.0);
+    } else if keys.just_pressed(KeyCode::PageDown) {
+        camera_transform.scale -= Vec3::new(0.2, 0.2, 0.0);
+    }
 }
