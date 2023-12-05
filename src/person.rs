3aba25597d91aa0890952aa01599a69d8fc47f87
Cyprien <cyprien.gille@orange.fr>
2023-12-05 16:51:37 +0100

    Add first person to avoid waiting for timer

--- src/person.rs
+++ src/person.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 use crate::{movement::MovementDir, states::GameState};
 
-const SPRITE_SCALE: Vec3 = Vec3::new(3.0, 3.0, 0.0);
+const SPRITE_SCALE: Vec3 = Vec3::new(1.0, 1.0, 0.0);
 
 #[derive(Resource)]
 struct UsedPersons {
@@ -19,12 +19,15 @@ pub enum PersonState {
     LookingForFood,
 }
 
-#[derive(Component, Debug)]
+#[derive(Component, Debug, Default)]
 pub struct Person {
     pub id: i32,
     pub hunger: i32,
     pub state: PersonState,
     pub movement_direction: MovementDir,
+    pub movement_vector: Vec3,
+    pub liked: Vec<i32>,
+    pub disliked: Vec<i32>,
 }
 
 #[derive(Bundle)]
@@ -39,14 +42,43 @@ impl Plugin for PersonPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(UsedPersons { list: vec![] })
             .insert_resource(SpawnTimer(Timer::from_seconds(7.0, TimerMode::Repeating)))
-            .add_systems(Update, (spawn_person).run_if(in_state(GameState::Playing)));
+            .add_systems(OnEnter(GameState::Playing), spawn_first_person)
+            .add_systems(Update, (spawn_person).run_if(in_state(GameState::Playing)))
+            .add_systems(OnExit(GameState::Playing), cleanup_persons);
     }
+}
+
+fn spawn_first_person(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut used_ids: ResMut<UsedPersons>,
+) {
+    commands.spawn(PersonBundle {
+        person: Person {
+            id: 0,
+            hunger: 0,
+            state: PersonState::Idle,
+            movement_direction: MovementDir::PlusBoth,
+            movement_vector: Vec3::ZERO,
+            ..default()
+        },
+        sprite: SpriteBundle {
+            texture: asset_server.load("Adam_idle_front.png"),
+            transform: Transform {
+                scale: SPRITE_SCALE,
+                translation: Vec3::new(0.0, 0.0, 1.0),
+                ..default()
+            },
+            ..default()
+        },
+    });
+    used_ids.list.push(0);
 }
 
 fn spawn_person(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    used_ids: Res<UsedPersons>,
+    mut used_ids: ResMut<UsedPersons>,
     time: Res<Time>,
     mut spawn_timer: ResMut<SpawnTimer>,
 ) {
@@ -62,15 +94,25 @@ fn spawn_person(
                 hunger: 0,
                 state: PersonState::Idle,
                 movement_direction: MovementDir::PlusBoth,
+                movement_vector: Vec3::ZERO,
+                ..default()
             },
             sprite: SpriteBundle {
-                texture: asset_server.load("Adam_idle_16x16_front.png"),
+                texture: asset_server.load("Adam_idle_front.png"),
                 transform: Transform {
                     scale: SPRITE_SCALE,
+                    translation: Vec3::new(0.0, 0.0, 1.0),
                     ..default()
                 },
                 ..default()
             },
         });
+        used_ids.list.push(available_id);
+    }
+}
+
+fn cleanup_persons(mut commands: Commands, person_query: Query<Entity, With<Person>>) {
+    for entity in &person_query {
+        commands.entity(entity).despawn_recursive();
     }
 }
