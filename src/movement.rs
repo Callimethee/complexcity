7ad191ed822bebba5a017c00eadab10f32948c2b
Cyprien <cyprien.gille@orange.fr>
2023-12-06 00:26:16 +0100

    Add social movement and balance moves

--- src/movement.rs
+++ src/movement.rs
@@ -11,10 +11,11 @@ use crate::{
     states::GameState,
 };
 
-const MOVEMENT_SCALAR: f32 = 18.0;
-const INTERACTION_FORCE: f32 = 1.0;
+const MOVEMENT_SCALAR: f32 = 2.0;
+const INTERACTION_FORCE: f32 = 0.5;
+const IDLE_FORCE: f32 = 3.5;
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 pub enum MovementDir {
     #[default]
     PlusX,
@@ -56,7 +57,7 @@ impl Plugin for MovementPlugin {
         )
         .add_systems(
             Update,
-            move_idle_persons.run_if(in_state(GameState::Playing)),
+            (move_idle_persons, social_movement).run_if(in_state(GameState::Playing)),
         )
         .add_systems(
             PostUpdate,
@@ -88,24 +89,24 @@ fn move_idle_persons(
         if matches!(person.state, PersonState::Idle) {
             match person.movement_direction {
                 MovementDir::PlusX => {
-                    person.movement_vector.x += 1.0;
+                    person.movement_vector.x += IDLE_FORCE * 1.0;
                 }
                 MovementDir::PlusY => {
-                    person.movement_vector.y += 1.0;
+                    person.movement_vector.y += IDLE_FORCE * 1.0;
                 }
                 MovementDir::PlusBoth => {
-                    person.movement_vector.x += 1.0 / SQRT_2;
-                    person.movement_vector.y += 1.0 / SQRT_2;
+                    person.movement_vector.x += IDLE_FORCE * 1.0 / SQRT_2;
+                    person.movement_vector.y += IDLE_FORCE * 1.0 / SQRT_2;
                 }
                 MovementDir::MinusX => {
-                    person.movement_vector.x -= 1.0;
+                    person.movement_vector.x -= IDLE_FORCE * 1.0;
                 }
                 MovementDir::MinusY => {
-                    person.movement_vector.y -= 1.0;
+                    person.movement_vector.y -= IDLE_FORCE * 1.0;
                 }
                 MovementDir::MinusBoth => {
-                    person.movement_vector.x -= 1.0 / SQRT_2;
-                    person.movement_vector.y -= 1.0 / SQRT_2;
+                    person.movement_vector.x -= IDLE_FORCE * 1.0 / SQRT_2;
+                    person.movement_vector.y -= IDLE_FORCE * 1.0 / SQRT_2;
                 }
             }
         }
@@ -113,9 +114,9 @@ fn move_idle_persons(
 }
 
 fn move_relative_to(
-    mut moved_person: Person,
-    moved_transform: Transform,
-    destination: Transform,
+    moved_person: &mut Person,
+    moved_transform: &Transform,
+    destination: &Transform,
     towards: bool,
 ) {
     let direction_mult = match towards {
@@ -124,11 +125,30 @@ fn move_relative_to(
     };
     moved_person.movement_vector += direction_mult
         * INTERACTION_FORCE
-        * (destination.translation - moved_transform.translation);
+        * (destination.translation - moved_transform.translation).clamp_length(0.0, 5.0);
+}
+
+fn social_movement(mut person_query: Query<(&mut Person, &Transform)>) {
+    let mut combinations = person_query.iter_combinations_mut();
+    while let Some([(mut person_1, transform_1), (mut person_2, transform_2)]) =
+        combinations.fetch_next()
+    {
+        if person_1.liked.contains(&person_2.id) {
+            move_relative_to(&mut person_1, transform_1, transform_2, true);
+        } else if person_1.disliked.contains(&person_2.id) {
+            move_relative_to(&mut person_1, transform_1, transform_2, false);
+        }
+        if person_2.liked.contains(&person_1.id) {
+            move_relative_to(&mut person_2, transform_2, transform_1, true);
+        } else if person_2.disliked.contains(&person_1.id) {
+            move_relative_to(&mut person_2, transform_2, transform_1, false);
+        }
+    }
 }
 
 fn resolve_movements(mut person_query: Query<(&Person, &mut Transform)>, time: Res<Time>) {
     for (person, mut transform) in &mut person_query {
-        transform.translation += person.movement_vector * MOVEMENT_SCALAR * time.delta_seconds();
+        transform.translation +=
+            person.movement_vector.clamp_length_max(5.0) * MOVEMENT_SCALAR * time.delta_seconds();
     }
 }
