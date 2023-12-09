b26754fecc9108d48cb5befb8744b61cc9e1aed5
Cyprien <cyprien.gille@orange.fr>
2023-12-09 03:09:32 +0100

    Implement movement relative to buildings

--- src/movement.rs
+++ src/movement.rs
@@ -6,14 +6,12 @@ use rand::{
     Rng,
 };
 
-use crate::{
-    person::{Person, PersonState},
-    states::GameState,
-};
+use crate::{building::BuildingType, person::Person, states::GameState};
 
-const MOVEMENT_SCALAR: f32 = 2.0;
-const INTERACTION_FORCE: f32 = 0.5;
-const IDLE_FORCE: f32 = 4.5;
+const MOVEMENT_SCALAR: f32 = 2.2;
+const IDLE_INTERACT: f32 = 3.0;
+const SOCIAL_INTERACT: f32 = 1.5;
+const BUILDING_INTERACT: f32 = 5.0;
 
 #[derive(Debug, Default, Clone, Copy)]
 pub enum MovementDir {
@@ -57,7 +55,8 @@ impl Plugin for MovementPlugin {
         )
         .add_systems(
             Update,
-            (move_idle_persons, social_movement).run_if(in_state(GameState::Playing)),
+            (move_idle_persons, social_movement, desire_movement)
+                .run_if(in_state(GameState::Playing)),
         )
         .add_systems(
             PostUpdate,
@@ -73,41 +72,39 @@ fn reset_movement_vector(mut person_query: Query<&mut Person>) {
 }
 
 fn move_idle_persons(
-    mut person_query: Query<&mut Person>,
+    mut persons_query: Query<&mut Person>,
     time: Res<Time>,
     mut dir_timer: ResMut<IdleDirectionTimer>,
 ) {
     if dir_timer.0.tick(time.delta()).just_finished() {
         // change the directions at random
-        for mut person in &mut person_query {
+        for mut person in &mut persons_query {
             let new_dir: MovementDir = rand::random();
             person.movement_direction = new_dir;
         }
     }
 
-    for mut person in &mut person_query {
-        if matches!(person.state, PersonState::Idle) {
-            match person.movement_direction {
-                MovementDir::PlusX => {
-                    person.movement_vector.x += IDLE_FORCE * 1.0;
-                }
-                MovementDir::PlusY => {
-                    person.movement_vector.y += IDLE_FORCE * 1.0;
-                }
-                MovementDir::PlusBoth => {
-                    person.movement_vector.x += IDLE_FORCE * 1.0 / SQRT_2;
-                    person.movement_vector.y += IDLE_FORCE * 1.0 / SQRT_2;
-                }
-                MovementDir::MinusX => {
-                    person.movement_vector.x -= IDLE_FORCE * 1.0;
-                }
-                MovementDir::MinusY => {
-                    person.movement_vector.y -= IDLE_FORCE * 1.0;
-                }
-                MovementDir::MinusBoth => {
-                    person.movement_vector.x -= IDLE_FORCE * 1.0 / SQRT_2;
-                    person.movement_vector.y -= IDLE_FORCE * 1.0 / SQRT_2;
-                }
+    for mut person in &mut persons_query {
+        match person.movement_direction {
+            MovementDir::PlusX => {
+                person.movement_vector.x += IDLE_INTERACT * 1.0;
+            }
+            MovementDir::PlusY => {
+                person.movement_vector.y += IDLE_INTERACT * 1.0;
+            }
+            MovementDir::PlusBoth => {
+                person.movement_vector.x += IDLE_INTERACT * 1.0 / SQRT_2;
+                person.movement_vector.y += IDLE_INTERACT * 1.0 / SQRT_2;
+            }
+            MovementDir::MinusX => {
+                person.movement_vector.x -= IDLE_INTERACT * 1.0;
+            }
+            MovementDir::MinusY => {
+                person.movement_vector.y -= IDLE_INTERACT * 1.0;
+            }
+            MovementDir::MinusBoth => {
+                person.movement_vector.x -= IDLE_INTERACT * 1.0 / SQRT_2;
+                person.movement_vector.y -= IDLE_INTERACT * 1.0 / SQRT_2;
             }
         }
     }
@@ -118,37 +115,154 @@ fn move_relative_to(
     moved_transform: &Transform,
     destination: &Transform,
     towards: bool,
+    factor: f32,
 ) {
     let direction_mult = match towards {
         true => 1.0,
         false => -1.0,
     };
     moved_person.movement_vector += direction_mult
-        * INTERACTION_FORCE
-        * (destination.translation - moved_transform.translation).clamp_length(0.0, 5.0);
+        * factor
+        * (destination.translation - moved_transform.translation).clamp_length_max(5.0);
 }
 
-fn social_movement(mut person_query: Query<(&mut Person, &Transform)>) {
-    let mut combinations = person_query.iter_combinations_mut();
+fn social_movement(mut persons_query: Query<(&mut Person, &Transform)>) {
+    let mut combinations = persons_query.iter_combinations_mut();
     while let Some([(mut person_1, transform_1), (mut person_2, transform_2)]) =
         combinations.fetch_next()
     {
         if person_1.liked.contains(&person_2.id) {
-            move_relative_to(&mut person_1, transform_1, transform_2, true);
+            move_relative_to(
+                &mut person_1,
+                transform_1,
+                transform_2,
+                true,
+                SOCIAL_INTERACT,
+            );
         } else if person_1.disliked.contains(&person_2.id) {
-            move_relative_to(&mut person_1, transform_1, transform_2, false);
+            move_relative_to(
+                &mut person_1,
+                transform_1,
+                transform_2,
+                false,
+                SOCIAL_INTERACT,
+            );
         }
         if person_2.liked.contains(&person_1.id) {
-            move_relative_to(&mut person_2, transform_2, transform_1, true);
+            move_relative_to(
+                &mut person_2,
+                transform_2,
+                transform_1,
+                true,
+                SOCIAL_INTERACT,
+            );
         } else if person_2.disliked.contains(&person_1.id) {
-            move_relative_to(&mut person_2, transform_2, transform_1, false);
+            move_relative_to(
+                &mut person_2,
+                transform_2,
+                transform_1,
+                false,
+                SOCIAL_INTERACT,
+            );
         }
     }
 }
 
-fn resolve_movements(mut person_query: Query<(&Person, &mut Transform)>, time: Res<Time>) {
-    for (person, mut transform) in &mut person_query {
-        transform.translation +=
-            person.movement_vector.clamp_length_max(5.0) * MOVEMENT_SCALAR * time.delta_seconds();
+fn desire_movement(
+    mut persons_query: Query<(&mut Person, &Transform)>,
+    buildings_query: Query<(&BuildingType, &Transform)>,
+) {
+    for (mut person, p_transform) in &mut persons_query {
+        if person.hunger < 25.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::Restaurant, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+        if person.shelter < 10.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::House, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+        if person.health < 26.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::Hospital, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+        if person.social < 25.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::Forum, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+        if person.creativity < 20.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::Creative, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+        if person.sport < 20.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::Pool, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+        if person.entertained < 10.0 {
+            move_relative_to(
+                &mut person,
+                p_transform,
+                &get_closest_of_interest(p_transform, BuildingType::Cinema, &buildings_query),
+                true,
+                BUILDING_INTERACT,
+            );
+        }
+    }
+}
+
+fn get_closest_of_interest(
+    p_transform: &Transform,
+    desired_type: BuildingType,
+    buildings_query: &Query<(&BuildingType, &Transform)>,
+) -> Transform {
+    let mut closest_of_interest = p_transform;
+    let mut min_distance_of_interest = f32::MAX;
+    for (b_type, b_transform) in buildings_query {
+        if *b_type == desired_type {
+            let newest_distance = p_transform.translation.distance(b_transform.translation);
+            if newest_distance < min_distance_of_interest {
+                min_distance_of_interest = newest_distance;
+                closest_of_interest = b_transform;
+            }
+        }
+    }
+    *closest_of_interest
+}
+
+fn resolve_movements(mut person_query: Query<(&mut Person, &mut Transform)>, time: Res<Time>) {
+    for (mut person, mut transform) in &mut person_query {
+        person.movement_vector =
+            person.movement_vector.clamp_length_max(6.5) * MOVEMENT_SCALAR * time.delta_seconds();
+        transform.translation.x += person.movement_vector.x;
+        transform.translation.y += person.movement_vector.y;
     }
 }
